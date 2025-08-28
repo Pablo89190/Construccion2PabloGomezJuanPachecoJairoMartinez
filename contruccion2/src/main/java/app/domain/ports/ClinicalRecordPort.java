42d59ce06e09b39734b60cebd8ff92aeee53c169
Juan David <fluyumax39@gmail.com>
2025-08-27 21:38:09 -0500

    add three new class

--- contruccion2/src/main/java/app/domain/ports/ClinicalRecordPort.java
+++ contruccion2/src/main/java/app/domain/ports/ClinicalRecordPort.java
@@ -7,6 +7,6 @@ import app.domain.model.ClinicalRecord;
 public interface ClinicalRecordPort {
 	public void save (ClinicalRecord clinicalRecord) throws Exception;
 	public List<ClinicalRecord> findByPatient(Patient patient) throws Exception ;
-    
+	
 	
 }
---
+++ contruccion2/src/main/java/app/domain/services/SearchClinicalRecordByPatient.java
@@ -0,0 +1,17 @@
+package app.domain.services;
+
+import app.domain.model.ClinicalRecord;
+import app.domain.ports.ClinicalRecordPort;
+import java.util.List;
+
+
+public class SearchClinicalRecordByPatient {
+
+	private ClinicalRecord  clinicalRecordPort;
+	
+public void searchClinicalRecordByPatient (ClinicalRecord clinicalRecord) throws Exception {
+		
+		
+		
+}
+}
