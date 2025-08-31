59fc2d765f504290d4d115373931bf5ce9e8ae8e
Pablo Gómez <pabloandresgomeze@gmail.com>
2025-08-31 02:08:33 -0500

    add new services and usecase

--- contruccion2/src/main/java/app/domain/ports/PatientPort.java
+++ contruccion2/src/main/java/app/domain/ports/PatientPort.java
@@ -4,17 +4,14 @@ import app.domain.model.Patient;
 
 public interface PatientPort {
     
-<<<<<<< HEAD
+
     Patient findById(String string) throws Exception;   
-=======
+
     Patient findById(long id) throws Exception;   
->>>>>>> e5de97035147bada9c1d2d5aee69811e23add275
     
     Patient findByFullName(String fullName) throws Exception;
     
     void save(Patient patient) throws Exception;
-<<<<<<< HEAD
 }
-=======
 }
->>>>>>> e5de97035147bada9c1d2d5aee69811e23add275
+
