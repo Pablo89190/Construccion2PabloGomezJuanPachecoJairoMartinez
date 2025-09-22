f3564ba220c10b010210c4a1dbb7e719e3cba182
Pablo89190 <pabloandresgomeze@gmail.com>
2025-09-22 07:51:22 -0500

    SQL and tags

--- contruccion2/src/main/java/app/Software2Application.java
+++ contruccion2/src/main/java/app/Software2Application.java
@@ -1,12 +1,20 @@
 package app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Software2Application {
+public class Software2Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Software2Application.class, args);
+		
+	}
+	
+	@Override
+	//preguntar al profe, al ejecutar no se imprime en pantalla "Esoty vivo"
+	public void run(String...args) throws Exception {
+		System.out.println("Estoy vivo");
 	}
 }
